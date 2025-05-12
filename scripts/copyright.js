97fc226863a3f1959a7f0572339c142b3ae55237
Data, Et cetera <alana.sharp@dataetc.org>
2025-05-12 15:51:05 -0400

    a few last resources

---
+++ scripts/copyright.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const copyrightHtml = `
+        <h5>
+            <center>
+                <font color="grey">Copyright &copy; 2018-2025 Data, Et cetera</font>
+            </center>
+        </h5>
+    `;
+    document.getElementById("copyright").innerHTML = copyrightHtml;
+});
